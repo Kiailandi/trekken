c13ec779690976534f633ff77fabeae86a222eab
spoonman01 <luca.rospocher@studenti.unitn.it>
2016-08-17 16:10:12 +0200

    Aggiunto check Sensori presenti all avvio

--- Trekken/app/src/main/java/com/trekken/HiddenMain.java
+++ Trekken/app/src/main/java/com/trekken/HiddenMain.java
@@ -4,14 +4,29 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class HiddenMain extends Activity {
+
+    SensorManager sMng;
+    List<Sensor> sensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hidden_main);
+
+        //Manda via Log tutti i sensori presenti sul dispositivo
+        sMng = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorList = sMng.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor s: sensorList) {
+            Log.i(ACCESSIBILITY_SERVICE, s.getName());
+        }
 
         SharedPreferences sharedPref = this.getSharedPreferences("Preferences", Context.MODE_PRIVATE);
         String textData = sharedPref.getString("Logged", "no");
