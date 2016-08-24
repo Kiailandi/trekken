58beaca4aa768867d93dba96b7b29f264493f38a
spoonman01 <luca.rospocher@studenti.unitn.it>
2016-08-24 18:32:25 +0200

    Aggiunti tutti i metodi della MainActivity tranne onCreate(). In questo stato ci sono 2 errori ma solo per mancanza di checkPermission su 2 chiamate

--- Trekken/app/src/main/java/com/trekken/MainActivity.java
+++ Trekken/app/src/main/java/com/trekken/MainActivity.java
@@ -25,7 +25,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.SwitchCompat;
 import android.telephony.SmsManager;
 import android.util.Log;
-import android.util.StringBuilderPrinter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -47,9 +46,55 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.IntentSender;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, DialogInterface.OnCancelListener {
+        implements NavigationView.OnNavigationItemSelectedListener, DialogInterface.OnCancelListener, OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private NavigationView navigationView;
     private View headerLayout;
@@ -64,6 +109,210 @@ public class MainActivity extends AppCompatActivity
     private Handler pbHandler;
     private Runnable r;
     private Ringtone alarm;
+
+    EditText txtLog;
+    Button btnLog;
+    Button btnStart;
+    Button btnStop;
+    Button btnLoad;
+
+    ArrayList<LatLng> pathPoints;
+    Polyline line;
+    ArrayList<Float> pathPointsAccuracy;
+
+    GoogleApiClient googleApiClient;
+    LocationRequest mLocationRequest;
+    Location mCurrentLocation;
+    LocationSettingsRequest.Builder builder;
+
+    //Check invertire
+    static final long time_interval = 1000 * 5;      //Millisecondi
+    static final long fastest_time_interval = 1000 * 3;    //Millisecondi
+
+    String mLastUpdateTime;
+    GoogleMap gMap = null;
+    boolean afterOnConnected = false;
+
+    static final int lineWidth = 6;
+
+    File filepath;
+    FileWriter writer;
+
+    int waitToStart = 0;
+
+    String _pathLoad;
+
+    private ArrayList<LatLng> readFromFile() {
+        ArrayList<LatLng> ret = null;
+        BufferedReader inputStream = null;
+
+        try {
+            ret = new ArrayList<>();
+            inputStream = new BufferedReader(new FileReader(_pathLoad));
+
+            String receiveString;
+
+            while ((receiveString = inputStream.readLine()) != null) {
+
+                Log.d("Carica: ", receiveString);
+
+                if (receiveString.contains(";")) {
+                    String[] split = receiveString.split(";");
+                    ret.add(new LatLng(Double.valueOf(split[1]), Double.valueOf(split[2])));
+                }
+            }
+
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            Log.e("File", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("File", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+    protected void writeLogsAndroid(String msg) {
+        txtLog.append(msg);
+        Log.d("MapActivity", msg);
+    }
+
+    protected boolean checkGpsEnabled() {
+
+        String msg = "Per una maggiore precisione ti consigliamo di attivare il gps in modalità precisione elevata, vuoi abilitarlo?";
+        boolean gpsEnabled = ((LocationManager) this.getSystemService(LOCATION_SERVICE)).isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!gpsEnabled) {
+            AlertDialog ad = new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Attiva GPS")
+                    .setMessage(msg)
+                    .setPositiveButton("Vai a impostazioni", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Intent gpsOptionsIntent = new Intent(
+                                    android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(gpsOptionsIntent);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+        return ((LocationManager) this.getSystemService(LOCATION_SERVICE)).isProviderEnabled(LocationManager.GPS_PROVIDER) || ((LocationManager) this.getSystemService(LOCATION_SERVICE)).isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    protected boolean writeLogs() {
+        File root = new File(Environment.getExternalStorageDirectory(), "Notes");
+
+        if (!root.exists()) {
+            root.mkdirs();
+        }
+
+        filepath = new File(root, "Maps_Gps_LatLon" + ".txt");  // file path to save
+
+        try {
+            writer = new FileWriter(filepath);
+
+            if (pathPoints.size() > 0 && pathPoints != null) {
+                for (int i = 0; i < pathPoints.size(); i++)
+                    writer.append((i + 1) + ";" + pathPoints.get(i).latitude + ";" + pathPoints.get(i).longitude + ";" + pathPointsAccuracy.get(i) + "\r\n");
+
+                writer.flush();
+                Toast.makeText(getApplicationContext(), filepath.getName() + " creato! " + pathPoints.size() + " records", Toast.LENGTH_LONG).show();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(time_interval)
+                .setFastestInterval(fastest_time_interval);
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int result = googleAPI.isGooglePlayServicesAvailable(this);
+        if (result != ConnectionResult.SUCCESS) {
+            if (googleAPI.isUserResolvableError(result)) {
+                googleAPI.getErrorDialog(this, result, 9000).show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    protected void handleNewLocation(Location location) {
+        if (waitToStart == 0) {
+            txtLog.append(" \nhandleNewLocation .........");
+            mCurrentLocation = location;
+
+            if (mCurrentLocation != null) {
+                double lat = mCurrentLocation.getLatitude();
+                double lng = mCurrentLocation.getLongitude();
+
+                txtLog.append(" \n\nAt Time: " + mLastUpdateTime + "\n" +
+                        "Latitude: " + lat + "\n" +
+                        "Longitude: " + lng + "\n" +
+                        "Accuracy: " + mCurrentLocation.getAccuracy() + "\n" +
+                        "Provider: " + mCurrentLocation.getProvider() + "\n");
+
+                final LatLng currentPosition = new LatLng(lat, lng);
+
+                if (!afterOnConnected)
+                    gMap.animateCamera(CameraUpdateFactory.newLatLng(currentPosition));
+                else {
+                    gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 14));
+                    afterOnConnected = false;
+                }
+
+                pathPoints.add(currentPosition);
+                pathPointsAccuracy.add(mCurrentLocation.getAccuracy());
+
+                PolylineOptions options = new PolylineOptions().width(lineWidth).color(Color.RED).geodesic(true).addAll(pathPoints);
+                line = gMap.addPolyline(options);
+            } else {
+                txtLog.append(" \nLocation is null .........");
+
+                final LatLng besenello = new LatLng(45.940966, 11.1091463);
+                gMap.addMarker(new MarkerOptions().position(besenello).title("Marker a Besenello"));
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(besenello, 14));
+            }
+
+            txtLog.setSelection(txtLog.getText().length());
+        } else
+            waitToStart--;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == -1) {
+            _pathLoad = data.getStringExtra("GetPath") + "/" + data.getStringExtra("GetFileName");
+//            Toast.makeText(this, data.getStringExtra("GetPath") + "/" + data.getStringExtra("GetFileName"),
+//                    Toast.LENGTH_LONG).show();
+
+            ArrayList<LatLng> loadedPathPoint = readFromFile();
+            PolylineOptions options = new PolylineOptions().width(lineWidth).color(Color.RED).geodesic(true).addAll(loadedPathPoint);
+            line = gMap.addPolyline(options);
+        } else {
+            Toast.makeText(this, "Nessun file selezionato",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        txtLog.append(" \nonMapReady initiated .........");
+        gMap = map;
+        gMap.setMyLocationEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -156,12 +405,6 @@ public class MainActivity extends AppCompatActivity
 
                 //Testing stuff
                 final SwitchCompat swCompat = (SwitchCompat) findViewById(R.id.switchForActionBar);
-                if (!swCompat.isChecked()) {
-                    ImageView img = (ImageView) findViewById(R.id.imageView2);
-                    //img.setBackgroundColor(Color.BLUE);
-                    img.setImageResource(R.drawable.rospo);
-                }
-
                 swCompat.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -192,6 +435,24 @@ public class MainActivity extends AppCompatActivity
         //Metto Dysplay Name utente da DefaultSharedPreferences
         TextView txtName = (TextView) headerLayout.findViewById(R.id.textViewName);
         txtName.setText(dysplayName);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+            writeLogsAndroid(" \nonPause Location update stopped .........");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        writeLogsAndroid(" \nonResume .........");
+
+        googleApiClient.connect();
     }
 
     private void stopSensors() {
@@ -379,6 +640,45 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    //GoogleApiClient.ConnectionCallbacks provides call back for GoogleApiClient onConnected.
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        writeLogsAndroid(" \nonConnected - isConnected : " + googleApiClient.isConnected());
 
+        if (googleApiClient.isConnected()) {
+            if (checkGpsEnabled()) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);  //return this PendingResult<Status> pendingResult;
+                afterOnConnected = true;
+            }
+        }
+    }
 
+    //LocationListener provides call back for location change through onLocationChanged.
+    @Override
+    public void onLocationChanged(Location location) {
+        writeLogsAndroid(" \nFiring onLocationChanged .........");
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        handleNewLocation(location);
+    }
+
+    //GoogleApiClient.OnConnectionFailedListener provides call back for GoogleApiClient onConnectionFailed.
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, 9000);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            writeLogsAndroid(" \nConnection failed: " + connectionResult.toString());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        writeLogsAndroid(" \nonConnectionSuspended .........");
+    }
 }
+
