0ac1f59b4c5e9929ca627304a42eceaf84349481
spoonman01 <luca.rospocher@studenti.unitn.it>
2016-08-24 15:01:06 +0200

    Aggiunte alcune impostazioni colore

--- Trekken/app/src/main/java/com/trekken/SettingsActivity.java
+++ Trekken/app/src/main/java/com/trekken/SettingsActivity.java
@@ -4,6 +4,7 @@ package com.trekken;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -33,7 +34,7 @@ import java.util.List;
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class SettingsActivity extends AppCompatPreferenceActivity {
+public class SettingsActivity extends AppCompatPreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     /**
      * A preference value change listener that updates the preference's summary
      * to reflect its new value.
@@ -122,6 +123,26 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         setupActionBar();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+/*        if ( key.equals("color_list")) {
+
+        }*/
+        //TODO finish color change
+    }
+
 
     /**
      * Set up the {@link android.app.ActionBar}, if the API is available.
@@ -192,7 +213,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // guidelines.
             bindPreferenceSummaryToValue(findPreference("display_name"));
             bindPreferenceSummaryToValue(findPreference("emergency_number"));
-            bindPreferenceSummaryToValue(findPreference("example_list"));
+            bindPreferenceSummaryToValue(findPreference("friend_list"));
+            bindPreferenceSummaryToValue(findPreference("color_list"));
         }
 
         @Override
