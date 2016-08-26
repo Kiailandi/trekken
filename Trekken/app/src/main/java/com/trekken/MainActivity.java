48330ace38d380a9d449ee84fcdda293c1d80a0d
spoonman01 <luca.rospocher@studenti.unitn.it>
2016-08-26 17:03:45 +0200

    Pulizzia in Main

--- Trekken/app/src/main/java/com/trekken/MainActivity.java
+++ Trekken/app/src/main/java/com/trekken/MainActivity.java
@@ -14,8 +14,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -26,9 +24,7 @@ import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.SwitchCompat;
-import android.telephony.SmsManager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -38,18 +34,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.DecelerateInterpolator;
-import android.view.animation.RotateAnimation;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.IntentSender;
 import android.graphics.Color;
@@ -94,7 +83,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, DialogInterface.OnCancelListener, OnMapReadyCallback,
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
@@ -108,10 +97,6 @@ public class MainActivity extends AppCompatActivity
     private SensorEventListener listenerAccelerometer;
     private double rootSquare;
     static final double threshold = 3.0; //TODO rimettere 1.5 a fine test
-    private ProgressBar pb;
-    private Handler pbHandler;
-    private Runnable r;
-    private Ringtone alarm;
 
     EditText txtLog;
     Button btnLog;
@@ -543,117 +528,6 @@ public class MainActivity extends AppCompatActivity
     private void startSensors() {
         snrManager.registerListener(listenerAccelerometer, snrAccelerometer, SensorManager.SENSOR_DELAY_UI);
     }
-
-    private void fallDetected() {
-        //TODO farlo funzionare anche a cell bloccato
-        //Toast.makeText(this, "Fall detected", Toast.LENGTH_SHORT).show();
-
-        //AlertDialog setup, inflating his view and finding ProgressBar and Button
-        AlertDialog.Builder helpBuilder = new AlertDialog.Builder(this);
-        helpBuilder.setTitle("Fall Detected !");
-
-        LayoutInflater inflater = getLayoutInflater();
-        View popupLayout = inflater.inflate(R.layout.popup_timer, (ViewGroup) MainActivity.this.findViewById(R.id.popup_layout));
-        helpBuilder.setView(popupLayout);
-        final AlertDialog helpDialog = helpBuilder.create();
-
-        pb = (ProgressBar) popupLayout.findViewById(R.id.progressBarTimer);
-        AnimationSet anSet = new AnimationSet(true);
-
-        //Rotate 90 degrees
-        Animation anRotate = new RotateAnimation(0.0f, 90.0f, 250f, 273f);
-        anSet.addAnimation(anRotate);
-
-        //Move back to the right
-        Animation anTranslate = new TranslateAnimation(0f, 263f, 0f, 0f);
-        anSet.addAnimation(anTranslate);
-
-        anSet.setInterpolator(new DecelerateInterpolator());
-        anSet.setFillAfter(true);
-
-        //Animate the Progress Bar
-        anSet.start();
-        pb.startAnimation(anSet);
-
-        //Setting up the alarm sound
-        Uri uriAlarm;
-        defaultPref = PreferenceManager.getDefaultSharedPreferences(this);
-        String alarmRingtone = defaultPref.getString("notifications_alarm_ringtone", "notFound");
-        if (!alarmRingtone.equals("notFound")) {
-            uriAlarm = Uri.parse(alarmRingtone);
-        } else {
-            uriAlarm = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        }
-        alarm = RingtoneManager.getRingtone(getApplicationContext(), uriAlarm);
-
-        //Launching the Popup Dialog
-        helpDialog.setCancelable(false);
-        helpDialog.setOnCancelListener(this); //Just in case
-        helpDialog.show();
-
-        alarm.play();
-
-        // Handler functioning as a Timer with tick = 1s
-        // Decrease the value of the ProgressBar until stopped by Button or it will send an Emergency Sms
-        pbHandler = new Handler();
-        r = new Runnable() {
-            public void run() {
-                if (pb.getProgress() > 1) {
-                    pb.setProgress(pb.getProgress() - 1);
-                    pbHandler.postDelayed(this, 500);
-                }
-                else {
-                    pb.setProgress(45);
-                    sendEmergencySMS();
-                    alarm.stop();
-                    startSensors();
-                    helpDialog.dismiss();
-                    pbHandler.removeCallbacks(this);
-                }
-            }
-        };
-        pbHandler.removeCallbacks(r);
-        pbHandler.postDelayed(r, 500);
-
-        //User stops the Alarm
-        Button btnDismiss = (Button) popupLayout.findViewById(R.id.btnDismissPopup);
-        btnDismiss.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Reset the AlertDialog and its elements
-                pbHandler.removeCallbacks(r);
-                alarm.stop();
-                startSensors();
-                helpDialog.dismiss();
-            }
-        });
-    }
-
-    //Called in case user or Android cancel the AlertDialog
-    @Override
-    public void onCancel(DialogInterface dialog) {
-        //Reset the AlertDialog and its elements
-        pbHandler.removeCallbacks(r);
-        alarm.stop();
-        startSensors();
-    }
-    private void sendEmergencySMS() {
-        //Log.i("Send SMS", "");
-        String phoneNo = defaultPref.getString("emergency_number", "banana");
-        String user = defaultPref.getString("display_name", "banana");
-
-        String message = "Trekken user " + user + " might be in danger while hiking and has requested your aid!";
-
-        try {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(phoneNo, null, message, null, null);
-            Toast.makeText(this, "SMS sent.", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            Toast.makeText(this, "SMS failed!", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
-    }
-
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
